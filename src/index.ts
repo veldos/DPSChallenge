f6cb822dfad0de6fb00802f6d0f561513e72dfd3
veldos <broa0971@gmail.com>
2024-07-25 16:26:56 +0100

    final commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
+import express from 'express';
+import projectRoutes from './routes/projectRoutes';
+import reportRoutes from './routes/reportRoutes';
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
+const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+app.use('/api', projectRoutes);
+app.use('/api', reportRoutes);
+
+app.listen(3000, () => {
+	console.log('Server is running on http://localhost:3000');
 });
+
+export default app;
